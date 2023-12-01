47c859ed8122ebb77b7056cb3c876ed029bf02c4
sonicthehedgehog0 <ademirakimbekova@gmail.com>
2023-12-01 22:26:56 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ademi_rakimbekova_hw5_2m/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ademi_rakimbekova_hw5_2m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
